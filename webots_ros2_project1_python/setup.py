3fa774ae80ae5fede40c5b0f26f399baf047f13d
Quinn Correll <142636496+correlltechnologies@users.noreply.github.com>
2024-10-16 15:44:00 -0500

    Update setup.py

--- webots_ros2_project1_python/setup.py
+++ webots_ros2_project1_python/setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-package_name = 'webots_ros2_homework1_python'
+package_name = 'webots_ros2_project1_python'
 
 data_files = []
 data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
@@ -22,14 +22,14 @@ setup(
     data_files=data_files,
     install_requires=['setuptools', 'launch'],
     zip_safe=True,
-    maintainer='Monica Anderson-UA',
-    maintainer_email='anderson@ua.edu',
+    maintainer='Quinn Correll',
+    maintainer_email='qcorrell@crimson.ua.edu',
     description='Simulation for cs460/560 Homework 1',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'launch.frontend.launch_extension': ['launch_ros = launch_ros'],    
-        'console_scripts': ['webots_ros2_homework1_python = webots_ros2_homework1_python.webots_ros2_homework1_python:main']
+        'console_scripts': ['webots_ros2_project1_python = webots_ros2_project1_python.webots_ros2_project1_python:main']
     },
 
 )
